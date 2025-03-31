705937df26a0c57a79efdfa64db82a1778237a8c
mmms914 <mikhail.khadzhaev832@gmail.com>
2025-03-31 20:35:34 +0700

    fixed favourites

--- api/controller/user_controller.go
+++ api/controller/user_controller.go
@@ -35,6 +35,7 @@ func (uc *UserController) Get(w http.ResponseWriter, r *http.Request) {
 			HasPicture:  user.HasPicture,
 			Collections: user.Collections,
 			Statistics:  user.Statistics,
+			Favourite:   user.Favourite,
 		}
 		if user.Collections == nil {
 			userInfo.Collections = make([]string, 0)
